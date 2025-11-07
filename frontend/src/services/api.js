bccd2224a669ca7f7e4710b310254263486a24af
bananezaromeo <bananezaromeo@gmail.com>
2025-11-07 23:42:36 +0200

    Remove ignored files from repo

--- frontend/src/services/api.js
+++
@@ -1,9 +0,0 @@
-import axios from 'axios';
-
-const API = axios.create({
-  baseURL: process.env.REACT_APP_API_URL || 'http://localhost:4000',
-  timeout: 10000,
-});
-
-export const fetchPublicRequests = () => API.get('/api/public-requests');
-export default API;
